affb1b31a718f5588f8c80f4adedf6fbb77943a2
cenfun <cenfun@qq.com>
2022-10-17 21:44:11 +0800

    fix docs

--- docs/assets/main.js
+++ docs/assets/main.js
@@ -1,6 +1,8 @@
 (function(window) {
     'use strict';
 
+    const { Grid, VERSION } = window.turbogrid;
+
     const formatCode = function(code) {
 
         let html = code.innerHTML;
@@ -140,26 +142,17 @@
         return n;
     };
 
-    const initThemes = function(grid) {
+    const initThemes = function() {
 
-        const $theme = document.querySelector('.st_theme');
+        const $theme = document.querySelector('.st-theme');
         if (!$theme) {
             return;
-        }
-
-        if (!grid) {
-            grid = window.turbogrid && window.turbogrid.Grid;
-            if (!grid) {
-                console.error('Not found defined Grid');
-                return;
-            }
         }
 
         $theme.title = 'theme';
 
         const hash = window.getHash();
-
-        const allThemes = grid.getAllThemes();
+        const allThemes = Grid.getAllThemes();
         allThemes.forEach((theme) => {
             const $option = document.createElement('option');
             $option.setAttribute('value', theme);
@@ -275,8 +268,7 @@
     };
 
     window.initCommonEvents = function(grid) {
-        console.log(grid);
-        initThemes(grid);
+        //console.log(grid);
         initButtons(grid);
     };
 
@@ -314,9 +306,8 @@
 
 
     const showSource = () => {
-        const listCss = [].concat(Array.from(document.querySelectorAll('style')));
-
-        const listJs = [].concat(Array.from(document.querySelectorAll('script')));
+        const listCss = Array.from(document.querySelectorAll('style'));
+        const listJs = Array.from(document.querySelectorAll('script'));
 
         const css = listCss.map(function(elem) {
             if (elem.getAttribute('context')) {
@@ -349,15 +340,429 @@
         window.showPage(content);
     };
 
-    const initSource = function() {
-        const bt_source = document.querySelector('.bt_source');
-        if (!bt_source) {
+    const getPageId = function() {
+        const filename = window.location.pathname.split('/').pop();
+        if (filename) {
+            if (filename.endsWith('.html')) {
+                return filename.slice(0, -5);
+            }
+            return filename;
+        }
+        return 'index';
+    };
+
+    const getGridRows = function() {
+        return [{
+            id: 'index',
+            name: 'Getting Started',
+            selectable: true,
+            nameClassMap: 'tg-row-top'
+        }, {
+            id: 'api',
+            name: 'API reference',
+            selectable: true,
+            nameClassMap: 'tg-row-top'
+        }, {
+            name: 'Popular Demo',
+            subs: [{
+                id: 'formatter',
+                name: 'Formatter'
+            }, {
+                id: 'style',
+                name: 'Style'
+            }, {
+                id: 'tooltip',
+                name: 'Tooltip'
+            }, {
+                id: 'popover',
+                name: 'Popover'
+            }, {
+                id: 'scroll',
+                name: 'Scroll'
+            }, {
+                id: 'scrollbar',
+                name: 'Scrollbar'
+            }, {
+                id: 'sort',
+                name: 'Sort'
+            }, {
+                id: 'events',
+                name: 'Events'
+            }, {
+                id: 'lifecycle',
+                name: 'Lifecycle'
+            }, {
+                id: 'performance-test',
+                name: 'Performance Test'
+            }]
+        }, {
+            name: 'Row',
+            subs: [{
+                id: 'row-add-delete',
+                name: 'Row Add/Delete'
+            }, {
+                id: 'row-collapse',
+                name: 'Row Collapse'
+            }, {
+                id: 'row-filter',
+                name: 'Row Filter'
+            }, {
+                id: 'row-select',
+                name: 'Row Select'
+            }, {
+                id: 'row-select-limit',
+                name: 'Row Select Limit'
+            }, {
+                id: 'row-select-group',
+                name: 'Row Select Group'
+            }, {
+                id: 'row-number',
+                name: 'Row Number'
+            }, {
+                id: 'row-drag',
+                name: 'Row Drag'
+            }, {
+                id: 'row-move',
+                name: 'Row Move'
+            }, {
+                id: 'row-hover',
+                name: 'Row Hover'
+            }, {
+                id: 'row-height',
+                name: 'Row Height'
+            }, {
+                id: 'row-not-found',
+                name: 'Row Not Found'
+            }]
+        }, {
+            name: 'Column',
+            subs: [{
+                id: 'column-add-delete',
+                name: 'Column Add/Delete'
+            }, {
+                id: 'column-display',
+                name: 'Column Display'
+            }, {
+                id: 'column-set',
+                name: 'Column Set'
+            }]
+        }, {
+            name: 'Header',
+            subs: [{
+                id: 'header-display',
+                name: 'Header Display'
+            }, {
+                id: 'header-group',
+                name: 'Header Group'
+            }]
+        }, {
+            name: 'Data',
+            subs: [{
+                id: 'infinite-scroll',
+                name: 'Infinite Scroll'
+            }, {
+                id: 'load-cells',
+                name: 'Load Cells'
+            }, {
+                id: 'load-rows',
+                name: 'Load Rows'
+            }, {
+                id: 'load-subs',
+                name: 'Load Subs'
+            }, {
+                id: 'set-rows',
+                name: 'Set Rows'
+            }, {
+                id: 'skeleton-screen',
+                name: 'Skeleton Screen'
+            }, {
+                id: 'pagination',
+                name: 'Pagination'
+            }, {
+                id: 'loading',
+                name: 'Loading'
+            }, {
+                id: 'export',
+                name: 'Export'
+            }]
+        }, {
+            name: 'Integration',
+            subs: [{
+                id: 'vue-component',
+                name: 'Vue Component'
+            }, {
+                id: 'vue-integration',
+                name: 'Vue Integration'
+            }, {
+                id: 'custom-element',
+                name: 'Custom Element'
+            }]
+        }, {
+            name: 'Frozen',
+            subs: [{
+                id: 'frozen',
+                name: 'Frozen'
+            }, {
+                id: 'frozen-middle',
+                name: 'Frozen Middle'
+            }]
+        }, {
+            name: 'Other',
+            subs: [{
+                id: 'touch',
+                name: 'Touch'
+            }, {
+                id: 'resize',
+                name: 'Resize'
+            }, {
+                id: 'cache',
+                name: 'Cache'
+            }, {
+                id: 'auto-height',
+                name: 'Auto Height'
+            }, {
+                id: 'negative-number',
+                name: 'Negative Number'
+            }, {
+                id: 'multiple-instance',
+                name: 'Multiple Instance'
+            }, {
+                id: 'context-menu',
+                name: 'Context Menu'
+            }, {
+                id: 'online-render',
+                name: 'Online Render'
+            }, {
+                id: 'poc',
+                name: 'POC'
+            }, {
+                id: 'conflict',
+                name: 'Conflict Test'
+            }, {
+                id: 'snake-game',
+                name: 'Snake Game'
+            }, {
+                id: 'async',
+                name: 'Async Test'
+            }, {
+                id: 'other',
+                name: 'Other'
+            }]
+        }];
+    };
+
+    const initNavGrid = function() {
+        const grid = new Grid('.nav-grid');
+
+        grid.bind('onCellUpdated', function(e, d) {
+
+            if (this.renderSettings.type) {
+                // only for scroll
+                return;
+            }
+
+            const cellNode = d.node;
+            //console.log(d);
+            cellNode.classList.add('tg-cell-effect');
+
+        });
+
+        let scrollTimeId;
+        grid.bind('onScroll', function(e, d) {
+            clearTimeout(scrollTimeId);
+            scrollTimeId = setTimeout(function() {
+                localStorage.setItem('tg-scroll-top', d.scrollTop);
+            }, 500);
+        });
+
+        grid.bind('onClick', function(e, d) {
+
+            const rowItem = d.rowItem;
+            const id = rowItem.id;
+
+            if (!id) {
+                grid.toggleRow(rowItem);
+                return;
+            }
+
+            grid.setRowSelected(rowItem);
+
+            const pageId = getPageId();
+            if (id === pageId) {
+                window.location.reload();
+                return;
+            }
+            //console.log('iframe loading ...');
+
+            let url = `${id}.html`;
+            if (pageId !== 'api') {
+                url += window.location.hash;
+            }
+
+            window.location.href = url;
+
+        });
+
+        let keywords = '';
+        document.querySelector('.nav-keywords').addEventListener('keyup', function() {
+            const k = this.value;
+            if (k === keywords) {
+                return;
+            }
+            keywords = k;
+            //update rows
+            grid.update();
+        });
+
+        grid.setOption({
+            headerVisible: false,
+            selectMultiple: false,
+            scrollbarSize: 6,
+            scrollbarFade: true,
+            scrollbarRound: true,
+            bindWindowResize: true,
+            bindContainerResize: true,
+            frozenRow: 1,
+            frozenRowHoverable: true,
+            rowFilter: function(rowItem) {
+                if (!keywords) {
+                    return true;
+                }
+                if (rowItem.tg_frozen) {
+                    return true;
+                }
+                let name = rowItem.name;
+                if (name) {
+                    name = name.toLowerCase();
+                    if (name.indexOf(keywords) !== -1) {
+                        return true;
+                    }
+                }
+                return false;
+            },
+            rowNumberFilter: function(rowItem, i) {
+                if (rowItem.tg_group || rowItem.tg_frozen || rowItem.nameClassMap) {
+                    return false;
+                }
+                return true;
+            }
+        });
+
+        grid.setFormatter({
+            tree: function(value, rowItem, columnItem, cellNode) {
+                const defaultFormatter = this.getDefaultFormatter('tree');
+                const rn = `<div class="tg-tree-row-number">${rowItem.tg_row_number}</div>`;
+                return rn + defaultFormatter(value, rowItem, columnItem, cellNode);
+            }
+        });
+
+        const rows = getGridRows();
+
+        const pageId = getPageId();
+
+        rows.forEach(function(row) {
+            if (!row.subs) {
+                if (row.id === pageId) {
+                    row.selected = true;
+                }
+                return;
+            }
+            row.subs.forEach(function(sub) {
+                if (sub.id === pageId) {
+                    sub.selected = true;
+                }
+            });
+        });
+
+        grid.setData({
+            columns: [{
+                id: 'name',
+                name: 'Name',
+                width: 195
+            }],
+            rows: rows
+        });
+
+        let scrollTop = localStorage.getItem('tg-scroll-top');
+        if (scrollTop) {
+            scrollTop = parseInt(scrollTop);
+        }
+
+        grid.render({
+            scrollTop
+        });
+
+    };
+
+    const initSource = function($header) {
+        if (!document.querySelector('.grid-container')) {
             return;
         }
-        bt_source.title = 'Check source codes';
-        bt_source.addEventListener('click', function() {
+
+        $header.insertAdjacentHTML('beforeend', '<button class="bt-source">source</button>');
+        const btSource = document.querySelector('.bt-source');
+        btSource.title = 'Check demo source codes';
+        btSource.addEventListener('click', function() {
             showSource();
         });
+
+    };
+
+    const initNav = function() {
+
+        const headerTitle = `
+            <a class="header-title" href="/">TurboGrid</a> 
+            <a class="header-version" href="https://github.com/cenfun/turbogrid" target="_blank">v${VERSION}</a>
+        `;
+
+        //header
+        const $header = document.querySelector('.header');
+        $header.insertAdjacentHTML('afterbegin', `
+            ${headerTitle}
+            <div class="icon icon-menu header-icon-menu"></div>
+            <div class="flex-auto"></div>
+        `);
+
+        initSource($header);
+
+        //nav header
+        const $nav = document.createElement('div');
+        $nav.className = 'nav flex-column';
+        $nav.innerHTML = `
+            <div class="header flex-row">
+                ${headerTitle}
+                <div class="flex-auto"></div>
+                <div class="icon icon-close header-icon-close"></div>
+            </div>
+            <div class="nav-grid flex-auto"></div>
+            <div class="nav-search">
+                <input class="nav-keywords" value="" onfocus="this.select();" placeholder="Search Demo" />
+            </div>
+        `;
+
+        const $body = document.querySelector('.body');
+        $body.insertBefore($nav, $body.firstChild);
+
+        ['.header-icon-menu', '.header-icon-close'].forEach(function(selector) {
+            document.querySelector(selector).addEventListener('click', function(e) {
+                const cls = $nav.classList;
+                if (cls.contains('nav-opened')) {
+                    cls.remove('nav-opened');
+                    cls.add('nav-closed');
+                    $nav.addEventListener('animationend', function() {
+                        $nav.classList.remove('nav-closed');
+                        console.log('animationend');
+                    }, {
+                        once: true
+                    });
+                } else {
+                    cls.remove('nav-closed');
+                    cls.add('nav-opened');
+                }
+            });
+        });
+
+        initNavGrid();
 
     };
 
@@ -370,32 +775,8 @@
         }
     };
 
-    const initHeaderTitle = function() {
-        const headerTitle = document.querySelector('.header-title');
-        if (!headerTitle) {
-            return;
-        }
-        if (window.parent === window) {
-            headerTitle.classList.add('header-title-disable');
-            return;
-        }
-        headerTitle.title = 'Open in new window';
-        headerTitle.style.cursor = 'pointer';
-        headerTitle.addEventListener('click', function() {
-            let url = location.href;
-            const hash = window.getHash();
-            delete hash.page;
-            const usp = new URLSearchParams(hash);
-            const str = usp.toString();
-            if (str) {
-                url += `#${str}`;
-            }
-            window.open(url);
-        });
-    };
-
     const initDataSelect = function() {
-        const dataSelect = document.querySelector('.st_data');
+        const dataSelect = document.querySelector('.st-data');
         if (!dataSelect) {
             return;
         }
@@ -414,9 +795,9 @@
     };
 
     window.addEventListener('load', function() {
-        initSource();
+        initNav();
+        initThemes();
         initLogs();
-        initHeaderTitle();
         initDataSelect();
     });
 

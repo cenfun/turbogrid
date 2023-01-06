ed0cd3dd23e16f7e53d4c5c1fd4e249ea8fa714d
cenfun <cenfun@qq.com>
2023-01-06 15:15:38 +0800

    fix style lint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,16 +1,3 @@
 module.exports = {
-    extends: ["stylelint-config-recommended", "stylelint-config-standard"],
-    overrides: [{
-        customSyntax: "postcss-scss",
-        files: ["**/*.scss"]
-    }, {
-        customSyntax: "postcss-html",
-        files: ["*.html", "**/*.html"]
-    }],
-    rules: {
-        "at-rule-no-unknown": null,
-        "font-family-no-missing-generic-family-keyword": null,
-        "import-notation": null,
-        "indentation": 4
-    }
+    extends: ["stylelint-config-plus"]
 };

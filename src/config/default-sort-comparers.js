0766c17372e689fc110ebf047e3ca82242f1c474
cenfun <cenfun@gmail.com>
2023-03-21 20:34:34 +0800

    add boolean comparer

--- src/config/default-sort-comparers.js
+++ src/config/default-sort-comparers.js
@@ -1,14 +1,14 @@
 import Util from '../core/util.js';
-const emptyMatch = function(value) {
-    if (!value && value !== 0) {
+const isEmpty = function(value) {
+    if (value === '') {
         return true;
     }
     return false;
 };
 
-const emptyComparer = function(av, bv, options) {
-    const ae = emptyMatch(av);
-    const be = emptyMatch(bv);
+const emptyComparer = function(av, bv) {
+    const ae = isEmpty(av);
+    const be = isEmpty(bv);
 
     if (ae && be) {
         return 0;
@@ -33,7 +33,7 @@ const isNull = function(value) {
     return false;
 };
 
-const blankComparer = function(av, bv, options) {
+const blankComparer = function(av, bv) {
     const an = isNull(av);
     const bn = isNull(bv);
 
@@ -49,25 +49,25 @@ const blankComparer = function(av, bv, options) {
         return -1;
     }
 
-    return emptyComparer(av, bv, options);
+    return emptyComparer(av, bv);
 
 };
 
 // =======================================================================
 
 // tg_index is require be created every time
-const indexComparer = function(a, b, options) {
+const indexComparer = function(a, b) {
     return a.tg_index > b.tg_index ? 1 : -1;
 };
 
 // index comparer if value equal
-const equalComparer = function(a, b, options) {
-    return indexComparer(a, b, options);
+const equalComparer = function(a, b) {
+    return indexComparer(a, b);
 };
 
 // =======================================================================
 
-const stringComparer = function(av, bv, options) {
+const stringComparer = function(av, bv) {
     const ai = typeof av === 'string';
     const bi = typeof bv === 'string';
     // both are string
@@ -82,27 +82,27 @@ const stringComparer = function(av, bv, options) {
     return av > bv ? -1 : 1;
 };
 
-const diffTypeComparer = function(ai, bi, av, bv, options) {
+const diffTypeComparer = function(ai, bi, av, bv) {
     if (ai) {
         return -1;
     }
     if (bi) {
         return 1;
     }
-    return stringComparer(av, bv, options);
+    return stringComparer(av, bv);
 };
 
-const numberComparer = function(av, bv, options) {
+const numberComparer = function(av, bv) {
     const ai = typeof av === 'number';
     const bi = typeof bv === 'number';
     // both are number
     if (ai && bi) {
         return av > bv ? -1 : 1;
     }
-    return diffTypeComparer(ai, bi, av, bv, options);
+    return diffTypeComparer(ai, bi, av, bv);
 };
 
-const dateComparer = function(av, bv, options) {
+const dateComparer = function(av, bv) {
     const ad = new Date(av);
     const bd = new Date(bv);
     const ai = Util.isDate(ad);
@@ -117,7 +117,17 @@ const dateComparer = function(av, bv, options) {
         return am > bm ? -1 : 1;
 
     }
-    return diffTypeComparer(ai, bi, av, bv, options);
+    return diffTypeComparer(ai, bi, av, bv);
+};
+
+const booleanComparer = function(av, bv) {
+    const ai = typeof av === 'boolean';
+    const bi = typeof bv === 'boolean';
+    // both are boolean
+    if (ai && bi) {
+        return av > bv ? -1 : 1;
+    }
+    return diffTypeComparer(ai, bi, av, bv);
 };
 
 // =======================================================================
@@ -125,7 +135,7 @@ const dateComparer = function(av, bv, options) {
 const valueComparer = function(a, b, options, diffValueComparer) {
     const av = a[options.sortField];
     const bv = b[options.sortField];
-    const bc = blankComparer(av, bv, options);
+    const bc = blankComparer(av, bv);
     // 1, -1
     if (bc) {
         return options.sortBlankFactor * bc;
@@ -137,15 +147,15 @@ const valueComparer = function(a, b, options, diffValueComparer) {
             return options.sortFactor * dvc;
         }
     }
-    return equalComparer(a, b, options);
+    return equalComparer(a, b);
 };
 
 export default {
     // value: empty string
-    empty: emptyComparer,
+    emptyValue: emptyComparer,
 
     // value: null undefined, empty string
-    blank: blankComparer,
+    blankValue: blankComparer,
 
     // object: index is alias of equal
     equal: equalComparer,
@@ -154,16 +164,26 @@ export default {
     // object: diff value
     value: valueComparer,
 
+    diffType: diffTypeComparer,
+
     string: function(a, b, options) {
         return valueComparer(a, b, options, stringComparer);
     },
+    stringValue: stringComparer,
 
     number: function(a, b, options) {
         return valueComparer(a, b, options, numberComparer);
     },
+    numberValue: numberComparer,
 
     date: function(a, b, options) {
         return valueComparer(a, b, options, dateComparer);
-    }
+    },
+    dateValue: dateComparer,
+
+    boolean: function(a, b, options) {
+        return valueComparer(a, b, options, booleanComparer);
+    },
+    booleanValue: booleanComparer
 
 };

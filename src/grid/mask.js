c5bf130586e76d1448f4b7d7eab1e29cbded02e4
cenfun <cenfun@gmail.com>
2024-06-04 16:18:10 +0800

    add DOC for showMask

--- src/grid/mask.js
+++ src/grid/mask.js
@@ -3,25 +3,27 @@ import Util from '../core/util.js';
 export default {
 
     showMask: function(styleMap) {
-
-        const $mask = this.$container.find('.tg-mask');
-        const node = $mask.get(0);
-        if (styleMap && node) {
-            const cssText = Util.styleMap(styleMap);
-            if (cssText) {
-                node.style.cssText = cssText;
+        if (!this.hasMask || styleMap) {
+            const $mask = this.$container.find('.tg-mask');
+            const node = $mask.get(0);
+            if (styleMap && node) {
+                const cssText = Util.styleMap(styleMap);
+                if (cssText) {
+                    node.style.cssText = cssText;
+                }
             }
+
+            $mask.show();
+            this.hasMask = true;
         }
-
-        $mask.show();
-        this.hasMask = true;
-
         return this;
     },
 
     hideMask: function() {
-        this.$container.find('.tg-mask').hide();
-        this.hasMask = false;
+        if (this.hasMask) {
+            this.$container.find('.tg-mask').hide();
+            this.hasMask = false;
+        }
         return this;
     }
 };
